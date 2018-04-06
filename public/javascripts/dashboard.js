2473c57f4cb4ac1bbefbdfc1a236c1ddac3bb342
njlefebv <njlefebv@cs.ucalgary.ca>
2018-04-06 01:11:06 -0600

    Debug date and (none)

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -46,8 +46,8 @@ $(function () {
         const fromDate = $('#datepickerFeatureS').data("datepicker").getFormattedDate('yyyy-mm-dd');
         const toDate = $('#datepickerFeatureF').data("datepicker").getFormattedDate('yyyy-mm-dd');
 
-        console.log("StartDate: " + fromDate);
-        console.log("EndDate: " + toDate);
+        //console.log("StartDate: " + fromDate);
+        //console.log("EndDate: " + toDate);
 
         if (name.length < 1) {
             alert("Must provide a name for new feature");
